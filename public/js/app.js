6b4b42fc9e15d11c09b209705eacc39424b49e1a
Deneuville Fabian <deneuville.fabian@live.fr>
2022-07-25 21:29:24 +0200

    Working on product description section

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,37 @@
 const slider = document.querySelector('.slider');
 const slides = Array.from(document.querySelectorAll('.slide'));
 const miniatures = Array.from(document.querySelectorAll('.miniature'));
+const descriptionSlider = document.querySelector('.product-description-slider');
+const descriptionButtons = Array.from(document.querySelectorAll('.menu-button'));
+let activeSlide = 0;
+checkActiveSlide();
 for (let i = 0; i < miniatures.length; i++) {
-    miniatures[i].addEventListener('click', () => moveSlider(i));
-    miniatures[i].addEventListener('keypress', () => moveSlider(i));
+    miniatures[i].addEventListener('click', () => moveSlider(slider, i, 6));
+    miniatures[i].addEventListener('keypress', () => moveSlider(slider, i, 6));
 }
-function moveSlider(index) {
-    const translateValue = -index * 100 / 6;
-    slider.style.transform = `translateX(${translateValue}%)`;
+for (let i = 0; i < descriptionButtons.length; i++) {
+    descriptionButtons[i].addEventListener('click', () => {
+        moveSlider(descriptionSlider, i, 3);
+        activeSlide = i;
+        checkActiveSlide();
+    });
+    descriptionButtons[i].addEventListener('keypress', () => {
+        moveSlider(descriptionSlider, i, 3);
+        activeSlide = i;
+        checkActiveSlide();
+    });
+}
+function moveSlider(sliderElement, index, numberOfSlides) {
+    const translateValue = -index * 100 / numberOfSlides;
+    sliderElement.style.transform = `translateX(${translateValue}%)`;
+}
+function checkActiveSlide() {
+    for (let i = 0; i < descriptionButtons.length; i++) {
+        if (i === activeSlide) {
+            descriptionButtons[i].style.border = "2px solid black";
+        }
+        else {
+            descriptionButtons[i].style.border = "2px solid #F5F5F5";
+        }
+    }
 }
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,12 +2,42 @@ const slider = document.querySelector('.slider') as HTMLDivElement;
 const slides : HTMLDivElement[] = Array.from(document.querySelectorAll('.slide') as NodeListOf<HTMLDivElement>);
 const miniatures : HTMLImageElement[] = Array.from(document.querySelectorAll('.miniature') as NodeListOf<HTMLImageElement>);
 
+const descriptionSlider = document.querySelector('.product-description-slider') as HTMLDivElement;
+const descriptionButtons : HTMLButtonElement[] = Array.from(document.querySelectorAll('.menu-button') as NodeListOf<HTMLButtonElement>);
+
+let activeSlide : number = 0;
+
+checkActiveSlide();
+
 for(let i = 0; i < miniatures.length; i++){
-    miniatures[i].addEventListener('click', () => moveSlider(i))
-    miniatures[i].addEventListener('keypress', () => moveSlider(i))
+    miniatures[i].addEventListener('click', () => moveSlider(slider, i, 6))
+    miniatures[i].addEventListener('keypress', () => moveSlider(slider, i, 6))
 }
 
-function moveSlider(index : number) : void {
-    const translateValue : number = -index * 100 / 6;
-    slider.style.transform = `translateX(${translateValue}%)`
+for(let i = 0; i < descriptionButtons.length; i++){
+    descriptionButtons[i].addEventListener('click', () => {
+        moveSlider(descriptionSlider, i, 3);
+        activeSlide = i;
+        checkActiveSlide();
+    });
+    descriptionButtons[i].addEventListener('keypress', () => {
+        moveSlider(descriptionSlider, i, 3);
+        activeSlide = i;
+        checkActiveSlide();
+    });
+}
+
+function moveSlider(sliderElement : HTMLDivElement, index : number, numberOfSlides : number) : void {
+    const translateValue : number = -index * 100 / numberOfSlides;
+    sliderElement.style.transform = `translateX(${translateValue}%)`;
+}
+
+function checkActiveSlide() : void {
+    for(let i = 0 ; i < descriptionButtons.length ; i++){
+        if(i === activeSlide){
+            descriptionButtons[i].style.border = "2px solid black";
+        } else {
+            descriptionButtons[i].style.border = "2px solid #F5F5F5";
+        }
+    }
 }
